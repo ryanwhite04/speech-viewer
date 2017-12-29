10c5224dc6b193d10a1fc6d2c139d770231d9d59
Ryan <ryanwhite04@gmail.com>
2017-12-30 06:52:21 +0800

    Generate curve coefficients from syllables

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import debug from 'debug'
-// import Mousetrap from 'mousetrap'
 import speakers from 'speech-samples/index.json'
 import { PolynomialRegression } from 'ml-regression'
-// https://github.com/mljs/regression
-// https://www.npmjs.com/package/js-regression
-// https://en.wikipedia.org/wiki/Feature_scaling
-// import Chart from './Chart.jsx';
 import {
   LineChart,
   Line,
@@ -20,201 +15,67 @@ import {
   YAxis,
  } from 'recharts';
 
-// function getAxisYDomain(from, to, ref, offset) {
-// 	const refData = data.slice(from-1, to);
-//   let [ bottom, top ] = [ refData[0][ref], refData[0][ref] ];
-//   refData.forEach( d => {
-//   	if ( d[ref] > top ) top = d[ref];
-//     if ( d[ref] < bottom ) bottom = d[ref];
-//   });
-  
-//   return [ (bottom|0) - offset, (top|0) + offset ]
-// }
-
 export default class App extends Component {
 
-state = {
-  width: 1440,
-  height: 360,
-  offset: 20,
-  sentence: 0,
-  speaker: 0,
-  tones: 0,
-  domain: [0, 'auto'],
-  log: debug('app'),
-}
+  // At the moment, the app is just for visualization
+  // To speed things up in future, all non-react/ui parts will move elsewhere
+  // That way it can process sentences on NODE.js backend
+  // TODO migrate the following to own file, probs in speech-samples repo
+  // - debug, speakers, and regression imports
+  // - curves, syllables, data, and constants from render
+  // - format, clean, get{Range|Tone|Syllable|Curve}, normalize, and train functions
+  // In speech samples repo, if done correctly, can still be imported and visualized
 
-// initialState = {
-//   data,
-//   left : 'dataMin',
-//   right : 'dataMax',
-//   refAreaLeft : '',
-//   refAreaRight : '',
-//   top : 'dataMax+1',
-//   bottom : 'dataMin-1',
-//   animation : true
-// }
+  state = {
+    width: 1440,
+    height: 360,
+    offset: 20,
+    sentence: 0,
+    speaker: 0,
+    tones: 0,
+    domain: [0, 'auto'],
+    log: debug('app'),
+  }
 
-// zoom() {  
-//   let { refAreaLeft, refAreaRight, data } = this.state;
+  render() {
 
-//   if ( refAreaLeft === refAreaRight || refAreaRight === '' ) {
-//     this.setState( () => ({
-//       refAreaLeft : '',
-//       refAreaRight : ''
-//     }) );
-//     return;
-//   }
+    const { width, height, offset, speaker, sentence, tones, domain, log } = this.state;
+    const { age, languages, gender, parents, sentences } = speakers[speaker];
+    const { frames, transcript: [hanzi, pinyin], tags } = sentences[sentence];
 
-//   // xAxis domain
-//   if ( refAreaLeft > refAreaRight ) 
-//       [ refAreaLeft, refAreaRight ] = [ refAreaRight, refAreaLeft ];
+    log('state', { width, height, offset, speaker, sentence, tones, domain, log })
+    log('speaker', { age, languages, gender, parents, sentences })
+    log('sentence', { frames, transcript: [hanzi, pinyin], tags })
 
-//   // yAxis domain
-//   const [ bottom, top ] = getAxisYDomain( refAreaLeft, refAreaRight, 'cost', 1 );
-  
-//   this.setState( () => ({
-//     refAreaLeft : '',
-//     refAreaRight : '',
-//     data : data.slice(),
-//     left : refAreaLeft,
-//     right : refAreaRight,
-//     bottom, top
-//   } ) );
-// };
+    const data = frames.map(format).reduce(clean, [])
+    const syllables = tags.reduce(getRange(data), []).map(getSyllable(pinyin))
+    const lines = tags.map(getLine(pinyin))
+    const areas = syllables.map(getArea(data))
+    const curves = syllables.map(getCurve(data))
 
-// zoomOut() {
-//   const { data } = this.state;
-//   this.setState( () => ({
-//     data : data.slice(),
-//     refAreaLeft : '',
-//     refAreaRight : '',
-//     left: 'dataMin',
-//     right: 'dataMax',
-//     top: 'dataMax+1',
-//     bottom: 'dataMin',
-//   }) );
-// }
+    log({ data, syllables, lines, areas, curves })
 
-render() {
-
-  // Declare functions to be used
-  // const {
-  //   zoomOut,
-  //   setState,
-  //   zoom,
-  // } = this;
-
-  // Declare all the variables to be used
-  const {
-    width,
-    height,
-    offset,
-    speaker,
-    sentence,
-    tones,
-    domain,
-    log,
-
-    // For zoomable
-    // data, barIndex, left, right, refAreaLeft, refAreaRight, top, bottom
-  } = this.state;
-
-  const {
-    age, languages, gender, parents,
-    sentences,
-  } = speakers[speaker];
-
-  const {
-    frames = [],
-    transcript: [hanzi = '', pinyin = ''],
-    tags = [],
-    ceiling,
-    maxnCandidates
-  } = sentences[sentence];
-
-  const data = frames.map(format).reduce(clean, [])
-  const syllables = tags.reduce(getRange(data), []).map(getSyllable(pinyin))
-  const lines = tags.map(getLine(pinyin))
-  const areas = syllables.map(getArea(data))
-
-  // const curves = syllables
-
-  //   // get only 4th tones
-  //   .filter(({ tone }) => tone === 4)
-
-  //   // try on just the first one for testing
-  //   .slice(0, 1)
-
-  //   .map((({ x, tone }), i) => {
-  //     let y = data
-  //       .slice(x, x[i+1] || 10)
-  //       .map(({ frequency }) => frequency)
-  //       .filter(frequency => frequency)
-
-  //     const max = Math.max(y)
-  //     const min = Math.min(y)
-
-  //     y = y.map(x => (x - min) / (max - min))
-
-
-  //     log({ y })
-
-  //     const x = [...new Array(y.length).keys()]
-  //     const regression = new PolynomialRegression(x, y, 3)
-  //     const syllable = pinyin.split(' ')[i]
-  //     const tone = parseInt(syllable.slice(-1))
-  //     return {
-  //       ...regression,
-  //       string: regression.toString(),
-  //       syllable,
-  //       tone,
-  //     }
-  //   })
-
-  // Log Everything!!!
-  log({ data, lines, syllables, areas })
-
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <h1 className="App-title">{hanzi}</h1>
-      </header> */}
-      {/* <a href="javascript: void(0);" onClick={zoomOut}>Zoom Out</a> */}
-      <pre className="Speaker">Speaker: {JSON.stringify({ age, languages, gender, parents }, null, 2)}</pre>
-      <pre className="Sentence">Sentence: {JSON.stringify({ hanzi, pinyin }, null, 2)}</pre>
-      <LineChart className="LineChart"
-        width={width}
-        height={height}
-        data={data}
-        // onMouseDown={({ activeLabel }) => setState({ refAreaLeft: activeLabel }) }
-        // onMouseMove={({ activeLabel }) => refAreaLeft && setState({ refAreaRight: activeLabel }) }
-        // onMouseUp={zoom}
-        >
-        <CartesianGrid />
-        <Tooltip cursor={{strokeDasharray: '3 3'}} />
-        <Line dot={false} type="monotone" dataKey="frequency" stroke="#8884d8" animationDuration={300} />
-        <XAxis allowDataOverflow={true} domain={domain} dataKey="name" type="number" name="frame" unit="cs" />
-        <YAxis allowDataOverflow={true} dataKey="frequency" type="number" name="frequency" unit="Hz" />
-
-        {/* {(refAreaLeft && refAreaRight) ? (<ReferenceArea yAxisId="1" x1={refAreaLeft} x2={refAreaRight}  strokeOpacity={0.3} /> ) : null } */}
-        {lines.map(({ label, ...props }) =>
-          <ReferenceLine {...props}>
-            <Label position="top">{label}</Label>
-          </ReferenceLine>
-        )}
-        {areas.map(({ label, ...props }) =>
-          <ReferenceArea {...props}>
-            <Label>{label}</Label>
-          </ReferenceArea>
-        )}
-      </LineChart>
-    </div>
-  )
-}
+    return (
+      <div className="App">
+        <pre className="Speaker">Speaker: {JSON.stringify({ age, languages, gender, parents }, null, 2)}</pre>
+        <pre className="Sentence">Sentence: {JSON.stringify({ hanzi, pinyin }, null, 2)}</pre>
+        <LineChart className="LineChart" width={width} height={height} data={data}>
+          <CartesianGrid />
+          <Tooltip cursor={{strokeDasharray: '3 3'}} />
+          <Line dot={false} type="monotone" dataKey="frequency" stroke="#8884d8" animationDuration={300} />
+          <XAxis allowDataOverflow={true} domain={domain} dataKey="name" type="number" name="Time" unit="0ms" />
+          <YAxis allowDataOverflow={true} dataKey="frequency" type="number" name="Frequency" unit="Hz" />
+          {lines.map(({ label, ...props }) => <ReferenceLine {...props}><Label position="top">{label}</Label></ReferenceLine>)}
+          {areas.map(({ label, ...props }) => <ReferenceArea {...props}><Label>{label}</Label></ReferenceArea>)}
+        </LineChart>
+      </div>
+    )
+  }
 }
 
 function format({ frequency }, name) {
+  // This will probably get changed in future, when only frequency is passed in
+  // TODO input will become (frequency, name) instead
   return ({
     frequency,
     name,
@@ -222,6 +83,9 @@ function format({ frequency }, name) {
 }
 
 function clean(data, { frequency, name }, i, frames) {
+  // Just removes small spikes of sounds that interfere with getRange
+  // any sounds spike less than 40ms will get flattened to 0hz
+  // TODO Not done well, could definitely be refactored
   let value = { frequency, name };
   if (i && i < frames.length - 2) {
     if (!data.slice(-1)[0].frequency) {
@@ -235,28 +99,43 @@ function clean(data, { frequency, name }, i, frames) {
 }
 
 function getRange(data, buffer = 20) {
+  // This can definitely be fine-tuned
+  // given an x value, the position of a click in a stream of frame frequencies
+  // it should return the correct bounds of the syllable
+  // The main speaker clicked "before" the syllable, so it is quite difficult to get it right
+  // Still seems to work quite well, as you can see in sentence 1, but has a few errors here and there for other sentences
+  // If things turn out badly, come back here to fine tune, and make it more robust, to end up with less errors for the ML
   return (scanned, x, i, tags) => {
 
     const prev = tags[i-1];
     const next = tags[i+1];
+
+    // Adds a little but onto the end incase the following click came before this one ended
     const region = data.slice(...[prev, next ? next + buffer : next]);
 
-    // console.log('getRange', scanned, i)
+    // This is the end of the previously calculated region, so it can't start before this
     const wall = i ? scanned[i - 1][1].name : 0
 
     const start = region.find(({ frequency, name }, i, data) => {
 
-      const first0InFirstHalf = data.slice(0, data.length / 2).indexOf(({ frequency }) => !frequency);
+      // There are still some improvements to be made here, and some other attempts as you can see.
+      // TODO check out this code and scavenge anything useful.
+      // first0InFirstHalf is the first 0 frequency value encountered before the midpoint of the previous and next click
+      // if there is a 0 here at all, it probably means that the click was before the end of the previous click
+      // const first0InFirstHalf = data.slice(0, data.length / 2).indexOf(({ frequency }) => !frequency);
       // if (data[0].frequency && first0InFirstHalf > -1) {
       //   return frequency !== 0 && i > first0InFirstHalf
       // } else {
       //   return frequency !== 0 && name > x
       // }
+
         return frequency !== 0 && name > x && name > wall
     }) || data[x]
 
     const end = region.find(({ frequency, name }, i, data) => {
-      // console.log('getEnd', { frequency, name, start })
+
+      // This isn't especially smart, there are probably a few weird cases where this doesn't work
+      // TODO make this smarter to account for more "weird" clicks
       return name > start.name && frequency === 0
     }) || data[next]
 
@@ -264,24 +143,34 @@ function getRange(data, buffer = 20) {
   }
 }
 
+function getTone(pinyin) {
+  // TODO just put this back where it's used, or convert the pinyin at the beginning to "tones"
+  return key => +pinyin.split(' ')[key].slice('-1')
+}
+
 function getSyllable(pinyin) {
+  // This just bundles the tone with the range in a format more easily consumed by everything else
   const keyToTone = getTone(pinyin)
   return ([{ name: x1 }, { name: x2 }], key) => ({
     x: [x1, x2],
     tone: keyToTone(key)
   })
 }
-function getTone(pinyin) {
-  return key => +pinyin.split(' ')[key].slice('-1')
-}
 
 function getLine(pinyin) {
+  // These show when the clicks took place
+  // As you can see, she didn't tag them "mid" syllable as she was suppose to
+  // If a sentence has all these lines mid syllable, the "getRange" function would need to be updated
   const keyToTone = getTone(pinyin);
   return (x, key) => {
     const tone = keyToTone(key)
     return ({
       x, key,
       label: key,
+
+      // The colors are suppose to be conventional colors for the tones apparently
+      // I learnt them from Michel Thomas audio tapes when I was learning Mandarin
+      // Not sure if they are actually conventions though, might make them better
       stroke: [
         null,
         'yellow',
@@ -295,6 +184,7 @@ function getLine(pinyin) {
 }
 
 function getArea(data) {
+  // These are the boxes that border the tone contour of each syllable
   return ({ x, tone }, key) => {
 
     const region = data.slice(...x).map(({ frequency }) => frequency)
@@ -319,4 +209,49 @@ function getArea(data) {
     }
   }
 }
+
+function normalize(min, max) {
+
+  // https://en.wikipedia.org/wiki/Feature_scaling
+  // Just makes the data a little better
+  // Not actually sure if this is necessary
+  // Might be better to scaled after the regression
+  return x => (x - min) / (max - min)
+}
+
+function getCurve(data, order = 3) {
+  // This regression isn't for prediction,
+  // It's just to get meaningful variables from each contour
+  // I can put the coefficients into the actual ML later
+  return ({ x: [start, end], tone }, key) => {
+
+    let y = data.slice(start, end)
+
+      // Don't need "name" property anymore, finally!
+      .map(({ frequency }) => frequency)
+
+      // Remove any 0 frequencies
+      .filter(frequency => frequency)
+
+
+    y = y.map(normalize(Math.min(...y), Math.max(...y)))
+
+    // Make new x values in case 0 frequencies were removed
+    // Scaled to values between 0 and 1
+    const x = [...new Array(y.length).keys()].map(x => x / y.length)
+    
+    // This is where the magic happens.
+    // 3rd order seems to work well,
+    const { coefficients } = new PolynomialRegression(x, y, order)
+
+    return { coefficients, tone }
+  }
+}
+
+// function train() {
+//   // https://github.com/mljs/regression
+//   // https://www.npmjs.com/package/js-regression
+//   // Still needs to be written
+//   // Will probably use one of the libraries above to train MLA based on curve coefficients
+// }
           
