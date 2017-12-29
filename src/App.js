934de71a63cf479d1ddcbd4918838c0c374b8a22
Ryan <ryanwhite04@gmail.com>
2017-12-30 03:39:23 +0800

    Outline frequency contour of each syllable

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import debug from 'debug'
-import Mousetrap from 'mousetrap'
-import speakers from 'speech-samples'
+// import Mousetrap from 'mousetrap'
+import speakers from 'speech-samples/index.json'
 import { PolynomialRegression } from 'ml-regression'
 // https://github.com/mljs/regression
 // https://www.npmjs.com/package/js-regression
@@ -20,57 +20,108 @@ import {
   YAxis,
  } from 'recharts';
 
+// function getAxisYDomain(from, to, ref, offset) {
+// 	const refData = data.slice(from-1, to);
+//   let [ bottom, top ] = [ refData[0][ref], refData[0][ref] ];
+//   refData.forEach( d => {
+//   	if ( d[ref] > top ) top = d[ref];
+//     if ( d[ref] < bottom ) bottom = d[ref];
+//   });
+  
+//   return [ (bottom|0) - offset, (top|0) + offset ]
+// }
 
-function slice(data) {
-  return (x, i, tags) => {
+export default class App extends Component {
 
-    const prev = tags[i = 1];
-    const next = tags[i + 1];
-    const context = data.slice(prev, next);
-
-    // you've got the x 
-
-    const x1 = (data.slice(x, next).find(({ frequency, name }) => {
-      return frequency !== 0 && name > x + 2
-    }) || { name: x }).name;
-
-    const x2 = (data.slice(x1, x2).reverse().find(({ frequency, name }) => {
-      return frequency === 0
-    }) || { name: x2 }).name;
-
-    console.log(x1, x2, data.slice(x1, x2))
-    
-    return [x1, x2]
-  }
-
+state = {
+  width: 1440,
+  height: 360,
+  offset: 20,
+  sentence: 0,
+  speaker: 0,
+  tones: 0,
+  domain: [0, 'auto'],
+  log: debug('app'),
 }
 
-export default class App extends Component { 
-  state = {
-    offset: 20,
-    sentence: 0,
-    speaker: 0,
-    tones: 0,
-    start: 0,
-    end: undefined,
-    log: debug('app'),
-  }
+// initialState = {
+//   data,
+//   left : 'dataMin',
+//   right : 'dataMax',
+//   refAreaLeft : '',
+//   refAreaRight : '',
+//   top : 'dataMax+1',
+//   bottom : 'dataMin-1',
+//   animation : true
+// }
+
+// zoom() {  
+//   let { refAreaLeft, refAreaRight, data } = this.state;
+
+//   if ( refAreaLeft === refAreaRight || refAreaRight === '' ) {
+//     this.setState( () => ({
+//       refAreaLeft : '',
+//       refAreaRight : ''
+//     }) );
+//     return;
+//   }
+
+//   // xAxis domain
+//   if ( refAreaLeft > refAreaRight ) 
+//       [ refAreaLeft, refAreaRight ] = [ refAreaRight, refAreaLeft ];
+
+//   // yAxis domain
+//   const [ bottom, top ] = getAxisYDomain( refAreaLeft, refAreaRight, 'cost', 1 );
   
-  render() {
+//   this.setState( () => ({
+//     refAreaLeft : '',
+//     refAreaRight : '',
+//     data : data.slice(),
+//     left : refAreaLeft,
+//     right : refAreaRight,
+//     bottom, top
+//   } ) );
+// };
+
+// zoomOut() {
+//   const { data } = this.state;
+//   this.setState( () => ({
+//     data : data.slice(),
+//     refAreaLeft : '',
+//     refAreaRight : '',
+//     left: 'dataMin',
+//     right: 'dataMax',
+//     top: 'dataMax+1',
+//     bottom: 'dataMin',
+//   }) );
+// }
+
+render() {
+
+  // Declare functions to be used
+  // const {
+  //   zoomOut,
+  //   setState,
+  //   zoom,
+  // } = this;
 
   // Declare all the variables to be used
   const {
+    width,
+    height,
     offset,
     speaker,
     sentence,
     tones,
-    start,
-    end,  
+    domain,
     log,
+
+    // For zoomable
+    // data, barIndex, left, right, refAreaLeft, refAreaRight, top, bottom
   } = this.state;
 
   const {
-    email, name, age, nationality, gender, proficiency,
+    age, languages, gender, parents,
     sentences,
   } = speakers[speaker];
 
@@ -82,50 +133,10 @@ export default class App extends Component {
     maxnCandidates
   } = sentences[sentence];
 
-  // Make the contour nicer
-  const data = frames
-    // .map(({ frequency }) => frequency)
-    .map((frequency, name) => ({ frequency, name }))
-    .slice(start, end)
-
-  // For showing when the clicks took place
-  const lines = tags.map((x, key) => ({ x, key, 
-    position: 'top',
-    value: key,
-  }))
-
-  // Make the tags nice, want { start, end, tone, key } for each
-  const syllables = tags.map(slice(data))
-    .map((x, i) => ({ x, tone: pinyin.split(' ')[i].slice('-1')}))
-
-  const areas = [
-    null,
-    'yellow',
-    'blue',
-    'red',
-    'black',
-    'green',
-  ].map((stroke, i) => syllables
-
-    // get only ith tones
-    .filter(({ tone }) => !i || tone === i)
-
-    .map(({ x, tone }, key) => {
-
-      const y = ['min', 'max'].map(v => Math[v](data.slice(...x).map(({ frequency }) => frequency)))
-      
-      return {
-        x1: x[0],
-        x2: x[1],
-        y1: y[0],
-        y2: y[1],
-        label: tone,
-        key: 'area' + key,
-        stroke,
-        strokeOpacity: 0.3,
-      }
-    })
-  )
+  const data = frames.map(format).reduce(clean, [])
+  const syllables = tags.reduce(getRange(data), []).map(getSyllable(pinyin))
+  const lines = tags.map(getLine(pinyin))
+  const areas = syllables.map(getArea(data))
 
   // const curves = syllables
 
@@ -161,31 +172,151 @@ export default class App extends Component {
   //     }
   //   })
 
+  // Log Everything!!!
+  log({ data, lines, syllables, areas })
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <h1 className="App-title">{hanzi}</h1>
-      </header>
-      <pre className="App-intro">Speaker: {JSON.stringify({
-        email, name, age, nationality, gender, proficiency,
-      }, null, 2)}</pre>
-      <LineChart width={1440} height={400} data={data}>
+      </header> */}
+      {/* <a href="javascript: void(0);" onClick={zoomOut}>Zoom Out</a> */}
+      <pre className="Speaker">Speaker: {JSON.stringify({ age, languages, gender, parents }, null, 2)}</pre>
+      <pre className="Sentence">Sentence: {JSON.stringify({ hanzi, pinyin }, null, 2)}</pre>
+      <LineChart className="LineChart"
+        width={width}
+        height={height}
+        data={data}
+        // onMouseDown={({ activeLabel }) => setState({ refAreaLeft: activeLabel }) }
+        // onMouseMove={({ activeLabel }) => refAreaLeft && setState({ refAreaRight: activeLabel }) }
+        // onMouseUp={zoom}
+        >
         <CartesianGrid />
         <Tooltip cursor={{strokeDasharray: '3 3'}} />
-        <XAxis dataKey="x" type="number" name="frame" unit="cs" />
-        <YAxis dataKey="y" type="number" name="frequency" unit="Hz" />
-        {lines.map(({ key, x, value, position }) => {
-          return (
-            <ReferenceLine key={'line' + key} x={x} stroke="green">
-              <Label position={position} offset={20}>{value}</Label>
-            </ReferenceLine>
-          )
-        })}
-        {areas[tones].map(props => <ReferenceArea {...props} />)}
-        <Line dot={false} type="monotone" dataKey="frequency" stroke="black" />
-        {/* <Line dot={false} type="monotone" dataKey="strength" stroke="yellow" /> */}
-        {/* <Line dot={false} type="monotone" dataKey="candidates" stroke="cyan" /> */}
+        <Line dot={false} type="monotone" dataKey="frequency" stroke="#8884d8" animationDuration={300} />
+        <XAxis allowDataOverflow={true} domain={domain} dataKey="name" type="number" name="frame" unit="cs" />
+        <YAxis allowDataOverflow={true} dataKey="frequency" type="number" name="frequency" unit="Hz" />
+
+        {/* {(refAreaLeft && refAreaRight) ? (<ReferenceArea yAxisId="1" x1={refAreaLeft} x2={refAreaRight}  strokeOpacity={0.3} /> ) : null } */}
+        {lines.map(({ label, ...props }) =>
+          <ReferenceLine {...props}>
+            <Label position="top">{label}</Label>
+          </ReferenceLine>
+        )}
+        {areas.map(({ label, ...props }) =>
+          <ReferenceArea {...props}>
+            <Label>{label}</Label>
+          </ReferenceArea>
+        )}
       </LineChart>
     </div>
   )
-}}
+}
+}
+
+function format({ frequency }, name) {
+  return ({
+    frequency,
+    name,
+  })
+}
+
+function clean(data, { frequency, name }, i, frames) {
+  let value = { frequency, name };
+  if (i && i < frames.length - 2) {
+    if (!data.slice(-1)[0].frequency) {
+      if (!frames[i + 1].frequency || !frames[i+2].frequency) {
+        value = { frequency: 0, name }
+      }
+    }
+  } 
+  data.push(value);
+  return data; 
+}
+
+function getRange(data, buffer = 20) {
+  return (scanned, x, i, tags) => {
+
+    const prev = tags[i-1];
+    const next = tags[i+1];
+    const region = data.slice(...[prev, next ? next + buffer : next]);
+
+    // console.log('getRange', scanned, i)
+    const wall = i ? scanned[i - 1][1].name : 0
+
+    const start = region.find(({ frequency, name }, i, data) => {
+
+      const first0InFirstHalf = data.slice(0, data.length / 2).indexOf(({ frequency }) => !frequency);
+      // if (data[0].frequency && first0InFirstHalf > -1) {
+      //   return frequency !== 0 && i > first0InFirstHalf
+      // } else {
+      //   return frequency !== 0 && name > x
+      // }
+        return frequency !== 0 && name > x && name > wall
+    }) || data[x]
+
+    const end = region.find(({ frequency, name }, i, data) => {
+      // console.log('getEnd', { frequency, name, start })
+      return name > start.name && frequency === 0
+    }) || data[next]
+
+    return [...scanned, [start, end]]
+  }
+}
+
+function getSyllable(pinyin) {
+  const keyToTone = getTone(pinyin)
+  return ([{ name: x1 }, { name: x2 }], key) => ({
+    x: [x1, x2],
+    tone: keyToTone(key)
+  })
+}
+function getTone(pinyin) {
+  return key => +pinyin.split(' ')[key].slice('-1')
+}
+
+function getLine(pinyin) {
+  const keyToTone = getTone(pinyin);
+  return (x, key) => {
+    const tone = keyToTone(key)
+    return ({
+      x, key,
+      label: key,
+      stroke: [
+        null,
+        'yellow',
+        'blue',
+        'red',
+        'black',
+        'green',
+      ][tone] || "#82ca9d",
+    })
+  }
+}
+
+function getArea(data) {
+  return ({ x, tone }, key) => {
+
+    const region = data.slice(...x).map(({ frequency }) => frequency)
+    const y = [Math.min(...region), Math.max(...region)]
+    
+    return {
+      x1: x[0],
+      x2: x[1] - 1,
+      y1: y[0],
+      y2: y[1],
+      label: tone,
+      key: 'area' + key,
+      stroke: [
+        null,
+        'yellow',
+        'blue',
+        'red',
+        'black',
+        'green',
+      ][tone],
+      strokeOpacity: 0.3,
+    }
+  }
+}
+          
